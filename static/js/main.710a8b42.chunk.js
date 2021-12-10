88bf5ac39e652185ebadbfe176078b7185a31887
Blake Carlson <blakecarlson14@gmail.com>
2021-12-09 21:47:55 -0600

    Updates

---
+++ static/js/main.710a8b42.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-website"]=this["webpackJsonpmy-website"]||[]).push([[0],{4:function(e,t,s){"use strict";s.r(t);s(0);var n=s(3),c=s.n(n),i=s(2);function r(){return Object(i.jsx)("h1",{children:"This is react!"})}c.a.render(Object(i.jsx)(r,{}),document.getElementById("root"))}},[[4,1,2]]]);
+//# sourceMappingURL=main.710a8b42.chunk.js.map
