511f310feffdc1d5376c69a1629197cf5815d808
Blake Carlson <blakecarlson14@gmail.com>
2021-12-22 19:39:17 -0600

    more updates

--- src/pages/projects/bitcoin-tester/BitcoinTester.js
+++ src/pages/projects/bitcoin-tester/BitcoinTester.js
@@ -5,85 +5,78 @@ import dayjs from 'dayjs'
 import { Dropdown } from 'semantic-ui-react'
 
 export default function BitcoinTester() {
-  const [coinPrice, setCoinPrice] = React.useState([])
+
+  const lookupTable  = [
+    {symbol: 'btc', id: 'bitcoin'},
+    {symbol: 'eth', id: 'ethereum'},
+    {symbol: 'ada', id: 'cardano'},
+    {symbol: 'dot', id: 'polkadot'},
+    {symbol: 'ltc', id: 'litecoin'},
+  ]
+
+  const [coinPrices, setCoinPrices] = React.useState([])
   const [yScale, setYScale] = React.useState('linear')
   const [coin, setCoin] = React.useState('btc')
 
   React.useEffect(() => {
-      async function getPrices() {
-          let fetchCoin = 'bitcoin'
-          switch(coin) {
-            case 'eth':
-              fetchCoin = 'ethereum'
-              break;
-            case 'dot':
-              fetchCoin = 'polkadot'
-              break;
-            default:
-              break;
-          }
-          const res = await fetch(`https://api.coingecko.com/api/v3/coins/${fetchCoin}/market_chart/range/?vs_currency=usd&from=946733027&to=1640157827`)
-          const data = await res.json()
-          
-          setCoinPrice(data.prices)
-          console.log(`coin ${coin}`)
-      }
-      getPrices()
+    async function getPrices() {
+      const time = Math.floor(Date.now() / 1000)
+
+      const coinId = lookupTable.filter(item => item.symbol === coin).map(item => item.id)
+
+      const res = await fetch(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart/range/?vs_currency=usd&from=946733027&to=${time}`)
+      const data = await res.json()
+      
+      setCoinPrices(data.prices)
+    }
+    getPrices()
   }, [coin])
 
   let result = [];
-  coinPrice.map(entry => {
+  coinPrices.map(entry => {
     let newDate = dayjs(entry[0]).format("MM/DD/YYYY");
     let obj = {date: newDate, price: entry[1]}
     result.push(obj)
   })
 
-  let textLabel = 'BTC'
-  switch(coin) {
-    case 'eth':
-      textLabel = 'ETH'
-      break;
-    case 'dot':
-      textLabel = 'DOT'
-      break;
-    default:
-      break;
-  }
-
   return(
-    <div>
-      <Dropdown text={textLabel} pointing className='link item'>
-        <Dropdown.Menu>
-            <Dropdown.Item onClick={() => setCoin('btc')}>BTC</Dropdown.Item>
-            <Dropdown.Item onClick={() => setCoin('eth')}>ETH</Dropdown.Item>
-            <Dropdown.Item onClick={() => setCoin('dot')}>DOT</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-      <Dropdown text={yScale === 'linear' ? 'Linear' : 'Log'} pointing className='link item'>
-        <Dropdown.Menu>
-            <Dropdown.Item onClick={() => setYScale('linear')}>Linear</Dropdown.Item>
-            <Dropdown.Item onClick={() => setYScale('log')}>Log</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-      <ResponsiveContainer height={600}>
-        <LineChart data={result}>
-          <XAxis 
-            dataKey="date"
+
+      <div>
+        <Dropdown text={coin.toUpperCase()} pointing className='link item'>
+          <Dropdown.Menu>
+            {lookupTable.map(lookupCoin => (
+              <Dropdown.Item onClick={() => setCoin(`${lookupCoin.symbol}`)}>{lookupCoin.symbol.toUpperCase()}</Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
+        <Dropdown text={yScale === 'linear' ? 'Linear' : 'Log'} pointing className='link item'>
+          <Dropdown.Menu>
+              <Dropdown.Item onClick={() => setYScale('linear')}>Linear</Dropdown.Item>
+              <Dropdown.Item onClick={() => setYScale('log')}>Log</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <ResponsiveContainer height={600}>
+          <LineChart 
+            data={result} 
+            margin={{ top: 10, right: 20, left: 20, bottom: 5 }}
+          >
+            <XAxis 
+              dataKey="date"
             />
-          <YAxis
-            scale={yScale === 'linear' ? 'linear' : 'log'}
-            domain={['auto', 'auto']}
+            <YAxis
+              scale={yScale === 'linear' ? 'linear' : 'log'}
+              domain={['auto', 'auto']}
             />
-          <Line  
-            dataKey="price"
-            stroke="#8884d8"
-            dot={false}
+            <Line  
+              dataKey="price"
+              stroke="#8884d8"
+              dot={false}
             />
-          <Tooltip 
-            formatter={value => `$${value.toFixed(2)}`}
+            <Tooltip 
+              formatter={value => `$${value.toFixed(2)}`}
             />
-        </LineChart>
-      </ResponsiveContainer>
-    </div>
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
   )
 }
