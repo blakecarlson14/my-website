3689c4d85406a4f159fc5fcbc1ad8a32b8257ee7
Blake Carlson <blakecarlson14@gmail.com>
2022-01-06 03:17:56 -0600

    add more features

--- src/pages/projects/mtg/Mtg.js
+++ src/pages/projects/mtg/Mtg.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import MtgForm from './components/MtgForm'
-import { ImageList, ImageListItem } from '@mui/material'
+import { ImageList, ImageListItem, inputUnstyledClasses } from '@mui/material'
 
 
 const Mtg = () => {
@@ -17,7 +17,7 @@ const Mtg = () => {
   };
 
   const handleSubmit = (input) => {
-    async function getScryfallCards (nextPage) {
+    function getScryfallCards (nextPage) {
       let colors=[]
       if (input.whiteCheck) {
         colors.push('w')
@@ -41,16 +41,105 @@ const Mtg = () => {
 
       let lookup = []
 
+      if (input.name !== '') {
+        lookup.push(`${input.name}`)
+      }
+
+      if (input.text !== '') {
+        lookup.push(encodeURIComponent(`oracle=${input.text}`))
+      }
+
+      if (input.type !== '') {
+        lookup.push(encodeURIComponent(`t=${input.type}`))
+      }
+
       if (colorString !== '') {
         if (input.andor === 'and') {
-          lookup.push(`c%3D${colorString}`)
+          lookup.push(encodeURIComponent(`c=${colorString}`))
         } else {
-          lookup.push(`c%3C%3D${colorString}`)
+          lookup.push(encodeURIComponent(`c<=${colorString}`))
         }
       }
 
-      if(input.cmc) {
-        lookup.push(`cmc%3D${input.cmc}`)
+      if(input.cmc !== '') {
+        lookup.push(encodeURIComponent(`cmc=${input.cmc}`))
+      }
+
+      if(input.power !== '') {
+        lookup.push(encodeURIComponent(`pow=${input.power}`))
+      }
+
+      if(input.toughness !== '') {
+        lookup.push(encodeURIComponent(`tou=${input.toughness}`))
+      }
+
+      if(input.format !== '') {
+        if(input.legal === 'legal') {
+          lookup.push(encodeURIComponent(`f=${input.format}`))
+        } else if (input.legal === 'restricted') {
+          lookup.push(encodeURIComponent(`restricted=${input.format}`))
+        } else {
+          lookup.push(encodeURIComponent(`banned=${input.format}`))
+        }
+      }
+
+      if(input.set !== '') {
+        lookup.push(encodeURIComponent(`e=${input.set}`))
+      }
+
+      let rarities=[]
+      
+      if (input.commonCheck) {
+        rarities.push('rarity%3Dc')
+      }
+      if (input.uncommonCheck) {
+        rarities.push('rarity%3Du')
+      }
+      if (input.rareCheck) {
+        rarities.push('rarity%3Dr')
+      }
+      if (input.mythicRareCheck) {
+        rarities.push('rarity%3Dm')
+      }
+
+      let rarityString = ''
+
+      rarityString = rarities.join('+OR+')
+
+      if(rarityString !== '') {
+        lookup.push(`%28${rarityString}%29`)
+      }
+
+      if (input.price !== '') {
+        let priceString = ''
+
+        console.log(`input.price: ${input.price}`)
+
+        console.log(`priceString1: ${priceString}`)
+
+        if (input.currency === 'usd') {
+          priceString = priceString.concat('usd')
+        } else if (input.currency === 'euro') {
+          priceString = priceString.concat('euro')
+        } else {
+          priceString = priceString.concat('tix')
+        }
+
+        console.log(`priceString2: ${priceString}`)
+
+        if (input.currencyCompare === 'lessThan') {
+          priceString = priceString.concat(`<${input.price}`)
+        } else if (input.currencyCompare === 'greaterThan') {
+          priceString = priceString.concat(`>${input.price}`)
+        } else if (input.currencyCompare === 'lessThanOrEqual') {
+          priceString = priceString.concat(`<=${input.price}`)
+        } else {
+          priceString = priceString.concat(`>=${input.price}`)
+        }
+
+        console.log(`priceString3: ${priceString}`)
+
+        lookup.push(encodeURIComponent(`${priceString}`))
       }
 
       let lookupString = lookup.join('+')
@@ -58,21 +147,30 @@ const Mtg = () => {
       if (lookupString !== '') {
         try {
           if (nextPage) {
-            const res = await fetch(nextPage)
-            const data = await res.json()
-            setCards( cards => {
-              return [...cards, ...data.data]
-            })
-            if (data.has_more) {
-              setTimeout(function(){getScryfallCards(data.next_page)}, 100)
-            }
+            fetch(nextPage)
+              .then(response => response.json())
+              .then(data => {
+                setCards( cards => {
+                  return [...cards, ...data.data]
+                })
+                if (data.has_more) {
+                  setTimeout(function(){getScryfallCards(data.next_page)}, 100)
+                } else {
+                  setIsFetching(false)
+                }
+              })
           } else {
-            const res = await fetch(`https://api.scryfall.com/cards/search?q=${lookupString}`)
-            const data = await res.json()
-            setCards(data.data)
-            if (data.has_more) {
-              setTimeout(function(){getScryfallCards(data.next_page)}, 100)
-            }
+            setIsFetching(true)
+            fetch(`https://api.scryfall.com/cards/search?q=${lookupString}`)
+              .then(response => response.json())
+              .then(data => {
+                setCards(data.data)
+                if (data.has_more) {
+                  setTimeout(function(){getScryfallCards(data.next_page)}, 100)
+                } else {
+                  setIsFetching(false)
+                }
+              })
           }
         }
         catch (error) {
@@ -140,29 +238,39 @@ const Mtg = () => {
         console.log('error')
       }
     }
-    setIsFetching(true)
     getScryfallCards()
-    setIsFetching(false)
   }
 
   if (cards && cards.length > 0) {
     return(
       <div>
-        <MtgForm handleSubmit={(e, input) => {
-          e.preventDefault()
-          handleSubmit(input)
-        }}/>
+        <MtgForm 
+          handleSubmit={(e, input) => {
+            e.preventDefault()
+            handleSubmit(input)
+          }}
+          isFetching={isFetching}
+        />
         <div>
           <ImageList cols={8}>
-          {console.log(`cards3: ${cards.length}`)}
           {cards.map(card => (
             card?.image_uris?.normal ?
                 <ImageListItem>
                   <img
                     src={card?.image_uris?.normal}
+                    loading="lazy"
                   />
                 </ImageListItem> :
-                null
+
+                card?.card_faces[0]?.image_uris.normal ?
+                  <ImageListItem>
+                  <img
+                    src={card?.card_faces[0]?.image_uris.normal}
+                    loading="lazy"
+                  />
+                  </ImageListItem> :
+
+                    null
             ))}
           </ImageList>
         </div> 
@@ -172,10 +280,13 @@ const Mtg = () => {
   else {
     return(
       <div>
-        <MtgForm handleSubmit={(e, input) => {
-          e.preventDefault()
-          handleSubmit(input)
-        }}/>
+        <MtgForm
+          handleSubmit={(e, input) => {
+            e.preventDefault()
+            handleSubmit(input)
+          }}
+          isFetching={isFetching}
+        />
         </div>)
   }
 }
--- src/pages/projects/mtg/components/MtgForm.js
+++ src/pages/projects/mtg/components/MtgForm.js
@@ -1,17 +1,35 @@
-import { Button, Checkbox, FormControl, FormControlLabel, FormLabel, Grid, InputLabel, MenuItem, Select, TextField } from '@mui/material'
+import { Button, Checkbox, FormControl, FormControlLabel, FormHelperText, FormLabel, Grid, InputAdornment, InputLabel, MenuItem, Select, TextField } from '@mui/material'
 import { Box } from '@mui/system'
+import { LoadingButton } from '@mui/lab'
 import React from 'react'
 
-const MtgForm = ({ handleSubmit }) => {
-  const [inputs, setInputs] = React.useState({ 
-    cmc: null,
-    whiteCheck: false,
-    blackCheck: false,
-    blueCheck: false,
-    greenCheck: false,
-    redCheck: false,
-    andor: 'and'
-  })
+const initialState = {
+  name: '',
+  text: '',
+  type: '',
+  whiteCheck: false,
+  blackCheck: false,
+  blueCheck: false,
+  greenCheck: false,
+  redCheck: false,
+  andor: 'and',
+  cmc: '',
+  power: '',
+  toughness: '',
+  format: '',
+  legal: 'legal',
+  set: '',
+  commonCheck: false,
+  uncommonCheck: false,
+  rareCheck: false,
+  mythicRareCheck: false,
+  currency: 'usd',
+  currencyCompare: 'lessThan',
+  price: '',
+}
+
+const MtgForm = ({ handleSubmit, isFetching }) => {
+  const [inputs, setInputs] = React.useState(initialState)
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -33,43 +51,224 @@ const MtgForm = ({ handleSubmit }) => {
     console.log(`name: ${name} value: ${checked}`)
   }
 
+  const resetForm = () => {
+    setInputs(initialState)
+  }
+
   return (
     <form onSubmit={ (e) => {
       handleSubmit(e, inputs) 
     }}>
-      <Box>
-        <FormControl sx={ { flexDirection: "row" } }>
-          <FormControlLabel control={ <Checkbox name='whiteCheck' checked={inputs.whiteCheck} onChange={handleCheck}/> } label="White" />
-          <FormControlLabel control={ <Checkbox name='blackCheck' checked={inputs.blackCheck} onChange={handleCheck}/> } label="Black" />
-          <FormControlLabel control={ <Checkbox name='greenCheck' checked={inputs.greenCheck} onChange={handleCheck}/> } label="Green" />
-          <FormControlLabel control={ <Checkbox name='redCheck' checked={inputs.redCheck} onChange={handleCheck}/> } label="Red" />
-          <FormControlLabel control={ <Checkbox name='blueCheck' checked={inputs.blueCheck} onChange={handleCheck}/> } label="Blue" />
-          <TextField 
-            value={inputs.andor}
-            onChange={handleChange}
-            select // tell TextField to render select
-            label="AND/OR"
-            name="andor"
-          >
-            <MenuItem value={'and'}>AND</MenuItem>
-            <MenuItem value={'or'}>OR</MenuItem>
-          </TextField>
-        </FormControl>
-      </Box>
-      <Box>
-        <FormControl>
-          <TextField
-            name="cmc"
-            type="text"
-            label="CMC"
-            onChange={ handleChange }
-          />
-        </FormControl>
-      </Box>
-      <Button variant="contained" color="primary" type="submit">
-        Submit
-      </Button>
-
+      <Grid>
+        <Grid item>
+          <FormControl>
+            <TextField
+              name="name"
+              value={inputs.name}
+              onChange={handleChange}
+              label="Name"
+              style = {{ margin: 10 }}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <FormControl>
+            <TextField
+              name="text"
+              value={inputs.text}
+              onChange={handleChange}
+              label="Text"
+              style = {{ margin: 10 }}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <FormControl>
+            <TextField
+              name="type"
+              value={inputs.type}
+              onChange={handleChange}
+              label="Type"
+              style = {{ margin: 10 }}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <FormControl sx={ { flexDirection: "row" } }>
+            <FormControlLabel control={ <Checkbox name='whiteCheck' checked={inputs.whiteCheck} onChange={handleCheck}/> } label="White" style = {{ marginLeft: 10 }}/>
+            <FormControlLabel control={ <Checkbox name='blueCheck' checked={inputs.blueCheck} onChange={handleCheck}/> } label="Blue" />
+            <FormControlLabel control={ <Checkbox name='blackCheck' checked={inputs.blackCheck} onChange={handleCheck}/> } label="Black" />
+            <FormControlLabel control={ <Checkbox name='redCheck' checked={inputs.redCheck} onChange={handleCheck}/> } label="Red" />
+            <FormControlLabel control={ <Checkbox name='greenCheck' checked={inputs.greenCheck} onChange={handleCheck}/> } label="Green" />
+            <TextField 
+              value={inputs.andor}
+              onChange={handleChange}
+              select
+              label="AND/OR"
+              name="andor"
+              style = {{ margin: 10 }}
+            >
+              <MenuItem value={'and'}>AND</MenuItem>
+              <MenuItem value={'or'}>OR</MenuItem>
+            </TextField>
+          </FormControl>
+        </Grid>
+        <Grid>
+          <FormControl>
+            <TextField
+              type="number"
+              name="cmc"
+              value={inputs.cmc}
+              onChange={ handleChange }
+              label="CMC"
+              style = {{ margin: 10 }}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <FormControl>
+            <TextField
+              type="number"
+              name="power"
+              value={inputs.power}
+              onChange={handleChange}
+              label="Power"
+              style = {{ margin: 10 }}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <FormControl>
+            <TextField
+              type="number"
+              name="toughness"
+              value={inputs.toughness}
+              onChange={handleChange}
+              label="Toughness"
+              style = {{ margin: 10 }}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <FormControl sx={ { flexDirection: "row" } } >
+            <TextField
+              select
+              name="format"
+              value={inputs.format}
+              onChange={handleChange}
+              label="Format"
+              style = {{ width: 150, marginLeft: 10, marginTop: 10, marginBottom: 10 }}
+            >
+              <MenuItem value={''}>&nbsp;</MenuItem>
+              <MenuItem value={'standard'}>Standard</MenuItem>
+              <MenuItem value={'historic'}>Historic</MenuItem>
+              <MenuItem value={'pioneer'}>Pioneer</MenuItem>
+              <MenuItem value={'modern'}>Modern</MenuItem>
+              <MenuItem value={'legacy'}>Legacy</MenuItem>
+              <MenuItem value={'pauper'}>Pauper</MenuItem>
+              <MenuItem value={'vintage'}>Vintage</MenuItem>
+            </TextField>
+            <TextField 
+              select
+              fullWidth
+              name="legal"
+              value={inputs.legal}
+              onChange={handleChange}
+              style = {{ width: 150, marginTop: 10, marginBottom: 10 }}
+            >
+              <MenuItem value={'legal'}>Legal</MenuItem>
+              <MenuItem value={'restricted'}>Restricted</MenuItem>
+              <MenuItem value={'banned'}>Banned</MenuItem>
+            </TextField>
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <FormControl>
+            <TextField
+              name="set"
+              value={inputs.set}
+              onChange={handleChange}
+              label="Set"
+              style = {{ margin: 10 }}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <FormControl sx={ { flexDirection: "row" } }>
+            <FormControlLabel control={ <Checkbox name='commonCheck' checked={inputs.commonCheck} onChange={handleCheck}/> } label="Common" style = {{ marginLeft: 10 }} />
+            <FormControlLabel control={ <Checkbox name='uncommonCheck' checked={inputs.uncommonCheck} onChange={handleCheck}/> } label="Uncommon" />
+            <FormControlLabel control={ <Checkbox name='rareCheck' checked={inputs.rareCheck} onChange={handleCheck}/> } label="Rare" />
+            <FormControlLabel control={ <Checkbox name='mythicRareCheck' checked={inputs.mythicRareCheck} onChange={handleCheck}/> } label="Mythic Rare" />
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <FormControl sx={ { flexDirection: "row" } } >
+            <TextField
+              select
+              name="currency"
+              value={inputs.currency}
+              onChange={handleChange}
+              style = {{ marginLeft: 10, marginTop: 10, marginBottom: 10 }}
+            >
+              <MenuItem value={'usd'}>USD</MenuItem>
+              <MenuItem value={'euro'}>Euros</MenuItem>
+              <MenuItem value={'tickets'}>MTGO Tickets</MenuItem>
+            </TextField>
+            <TextField 
+              select
+              name="currencyCompare"
+              value={inputs.currencyCompare}
+              onChange={handleChange}
+              style = {{ marginTop: 10, marginBottom: 10 }}
+            >
+              <MenuItem value={'lessThan'}>less than</MenuItem>
+              <MenuItem value={'greaterThan'}>greater than</MenuItem>
+              <MenuItem value={'lessThanOrEqual'}>less than or eqal</MenuItem>
+              <MenuItem value={'greaterThanOrEqual'}>greater than or eqal</MenuItem>
+            </TextField>
+            {inputs.currency === 'usd' ? 
+              <TextField
+              type="number"
+              name="price"
+              value={inputs.price}
+              onChange={handleChange}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">$</InputAdornment>
+                ),
+              }}
+              style = {{ marginTop: 10, marginBottom: 10 }}
+            /> : inputs.currency === 'euro' ? 
+            <TextField
+              type="number"
+              name="price"
+              value={inputs.price}
+              onChange={handleChange}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">€</InputAdornment>
+                ),
+              }}
+              style = {{ marginTop: 10, marginBottom: 10 }}
+            /> :
+            <TextField
+              type="number"
+              name="price"
+              value={inputs.price}
+              onChange={handleChange}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">TIX</InputAdornment>
+                ),
+              }}
+              style = {{ marginTop: 10, marginBottom: 10 }}
+            />
+            }
+          </FormControl>
+        </Grid>
+      </Grid>
+      {isFetching ? <LoadingButton loading variant="contained" style = {{ margin: 10 }}>Submit</LoadingButton> : <Button variant="contained" color="primary" type="submit" style = {{ margin: 10 }}>Submit</Button>}
+      <Button variant="contained" color="secondary" name="reset" style = {{ margin: 10 }} onClick={resetForm}>Reset</Button>
     </form>
   )
 }
