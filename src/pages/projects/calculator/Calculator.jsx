dcbdf9127b2998c8cac251a2a515a8440944caf4
Blake Carlson <blakecarlson14@gmail.com>
2023-03-31 23:51:12 -0500

    change class to className

--- src/pages/projects/calculator/Calculator.jsx
+++ src/pages/projects/calculator/Calculator.jsx
@@ -133,76 +133,76 @@ export const Calculator = () => {
   }
 
   return (
-    <div class='calculator'>
+    <div className='calculator'>
       <div>
-        <header class='calcScreen'>
+        <header className='calcScreen'>
           {screen}
         </header>
       </div>
       <div>
-        <button class='calcButton light' value='AC' onClick={(e) => handleClick(e.target.value)}>
+        <button className='calcButton light' value='AC' onClick={(e) => handleClick(e.target.value)}>
           {"AC"}
         </button>
-        <button class='calcButton light' value='+/-' onClick={(e) => handleClick(e.target.value)}>
+        <button className='calcButton light' value='+/-' onClick={(e) => handleClick(e.target.value)}>
           {'+/-'}
         </button>
-        <button class='calcButton light' value='%' onClick={(e) => handleClick(e.target.value)}>
+        <button className='calcButton light' value='%' onClick={(e) => handleClick(e.target.value)}>
           {'%'}
         </button>
-        <button class='calcButton orange' value='/' onClick={(e) => handleClick(e.target.value)}>
+        <button className='calcButton orange' value='/' onClick={(e) => handleClick(e.target.value)}>
           {'/'}
         </button>
       </div>
       <div>
-        <button class='calcButton dark' value='7' onClick={(e) => handleClick(e.target.value)}>
+        <button className='calcButton dark' value='7' onClick={(e) => handleClick(e.target.value)}>
           {'7'}
         </button>
-        <button class='calcButton dark' value='8' onClick={(e) => handleClick(e.target.value)}>
+        <button className='calcButton dark' value='8' onClick={(e) => handleClick(e.target.value)}>
           {'8'}
         </button>
-        <button class='calcButton dark' value='9' onClick={(e) => handleClick(e.target.value)}>
+        <button className='calcButton dark' value='9' onClick={(e) => handleClick(e.target.value)}>
           {'9'}
         </button>
-        <button class='calcButton orange' value='x' onClick={(e) => handleClick(e.target.value)}>
+        <button className='calcButton orange' value='x' onClick={(e) => handleClick(e.target.value)}>
           {'x'}
         </button>
       </div>
       <div>
-        <button class='calcButton dark' value='4' onClick={(e) => handleClick(e.target.value)}>
+        <button className='calcButton dark' value='4' onClick={(e) => handleClick(e.target.value)}>
           {'4'}
         </button>
-        <button class='calcButton dark' value='5' onClick={(e) => handleClick(e.target.value)}>
+        <button className='calcButton dark' value='5' onClick={(e) => handleClick(e.target.value)}>
           {'5'}
         </button>
-        <button class='calcButton dark' value='6' onClick={(e) => handleClick(e.target.value)}>
+        <button className='calcButton dark' value='6' onClick={(e) => handleClick(e.target.value)}>
           {'6'}
         </button>
-        <button class='calcButton orange' value='-' onClick={(e) => handleClick(e.target.value)}>
+        <button className='calcButton orange' value='-' onClick={(e) => handleClick(e.target.value)}>
           {'-'}
         </button>
       </div>
       <div>
-        <button class='calcButton dark' value='1' onClick={(e) => handleClick(e.target.value)}>
+        <button className='calcButton dark' value='1' onClick={(e) => handleClick(e.target.value)}>
           {'1'}
         </button>
-        <button class='calcButton dark' value='2' onClick={(e) => handleClick(e.target.value)}>
+        <button className='calcButton dark' value='2' onClick={(e) => handleClick(e.target.value)}>
           {'2'}
         </button>
-        <button class='calcButton dark' value='3' onClick={(e) => handleClick(e.target.value)}>
+        <button className='calcButton dark' value='3' onClick={(e) => handleClick(e.target.value)}>
           {'3'}
         </button>
-        <button class='calcButton orange' value='+' onClick={(e) => handleClick(e.target.value)}>
+        <button className='calcButton orange' value='+' onClick={(e) => handleClick(e.target.value)}>
           {'+'}
         </button>
       </div>
       <div>
-        <button class='calcButton dark double zero' value='0' onClick={(e) => handleClick(e.target.value)}>
+        <button className='calcButton dark double zero' value='0' onClick={(e) => handleClick(e.target.value)}>
           {'0'}
         </button>
-        <button class='calcButton dark' value='.' onClick={(e) => handleClick(e.target.value)}>
+        <button className='calcButton dark' value='.' onClick={(e) => handleClick(e.target.value)}>
           {'.'}
         </button>
-        <button class='calcButton orange' value='=' onClick={(e) => handleClick(e.target.value)}>
+        <button className='calcButton orange' value='=' onClick={(e) => handleClick(e.target.value)}>
           {'='}
         </button>
       </div>
